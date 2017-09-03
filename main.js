dac71aa67f702051b9b0b0f4b54ca1f015ad8d8e
msolorio <msolorio789@gmail.com>
2017-09-03 09:41:55 -0700

    attach event listeners with js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function dragStartHandler(e) {
-  console.log('dragStartHandler');
 
+
+function dragStartHandler(e) {
   e.dataTransfer.setData('text/plain', e.target.id);
 }
 
@@ -13,6 +13,29 @@ function dragOverHandler(e) {
 function dropHandler(e) {
   e.preventDefault();
 
-  var data = e.dataTransfer.getData('text');
+  var data = e.dataTransfer.getData('text/plain');
   e.target.appendChild(document.getElementById(data));
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  var draggableDiv = document.getElementById('draggableDiv');
+  var targetDiv = document.getElementById('target');
+
+  draggableDiv.addEventListener('dragstart', function(e) {
+    dragStartHandler(e);
+  });
+
+  draggableDiv.addEventListener('dragend', function(e) {
+    console.log('drag ended');
+  });
+
+  targetDiv.addEventListener('drop', function(e) {
+    dropHandler(e);
+  });
+
+  targetDiv.addEventListener('dragover', function(e) {
+    dragOverHandler(e);
+  });
+
+});
